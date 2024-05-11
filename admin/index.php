9dd606e1685b6584b77f42649d64076e5263504d
ajaykumar002 <ajay.openit@gmail.com>
2024-05-11 11:19:49 +0530

    Update title tag

--- admin/index.php
+++ admin/index.php
@@ -10,7 +10,7 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Registration form sangamam</title>
+		<title>Registration form Sangamam to Directory Details</title>
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
 		<link rel="stylesheet" href="../node_modules/mdbootstrap/css/bootstrap.min.css">
